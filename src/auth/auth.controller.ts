e1fea15cf440fe2557f1eaca854818a39dba591f
DonovanDumeige <donovan.dumeige@gmail.com>
2022-10-20 23:33:28 +0200

    feat: create logic for authentification
    Create method signup and signin on auth.controller and adequate routes.
    In auth service develop logic and validations for signup and signin method,
    according to auth dto.
    Add logic for hash and verify password.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+// This way allows to keep controllers clean and only busy with a logic to the requests.
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
